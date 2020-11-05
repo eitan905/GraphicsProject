bb375c8dcffe13f157c6cc9ce6e5c60cd5bbc524
eitan905 <eitan.sharabi@gmail.com>
2020-11-06 01:37:45 +0200

    .working on scene

--- Viewer/src/Renderer.cpp
+++ Viewer/src/Renderer.cpp
@@ -268,7 +268,8 @@ void Renderer::Render(const Scene& scene)
 
 
 
-	DrawModel(scene.GetModel(0));
+	DrawModel(scene.GetModel(0),scene);
+	
 
 	double thirty_degrees = (sqrt(3) / 2);
 	double fourty_five_degrees = (sqrt(2) / 2);
@@ -344,7 +345,7 @@ void Renderer::UseDrawLine(const glm::ivec2& p1, const glm::ivec2& p2, const glm
 	DrawLine(p1, p2, color);
 }
 
-void Renderer::DrawModel(MeshModel obj)
+void Renderer::DrawModel(MeshModel obj,const Scene& scene)
 {
 	for (int i = 0; i < obj.GetFacesCount(); i++) {
 		Face face = obj.GetFace(i);
